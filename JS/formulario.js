a2107c68b4235d3ba7a9ca85d620001b35b34ad4
Josu González <gonzalezlopezjosu@gmail.com>
2022-05-25 19:45:12 +0200

    6 commit

---
+++ JS/formulario.js
@@ -0,0 +1,9 @@
+var nombre,apellido;
+
+function obtenerDatos(){
+    nombre = document.getElementById("nombre").value;
+    apellido = document.getElementById("apellido").value;
+
+    var resultado = [nombre,apellido];
+    return r;
+}
